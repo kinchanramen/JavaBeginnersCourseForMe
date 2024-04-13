1ae920641328891943cc469e81d4f726a1efcba6
kinchanramen <kasuaho001@gmail.com>
2024-04-13 22:19:52 +0900

    ill sleep

---
+++ src/main/java/chapter_001/Exercise/Ex_001.java
@@ -0,0 +1,7 @@
+package chapter_001.Exercise;
+
+public class Ex_001 {
+    public static void main(String[] args) {
+        System.out.println("やっほ～～！！");
+    }
+}
---
+++ src/main/java/chapter_001/sample/sample_001.java
@@ -0,0 +1,7 @@
+package chapter_001.Exercise.sample;
+
+public class sample_001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
